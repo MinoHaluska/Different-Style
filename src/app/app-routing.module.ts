63c6192042daaf2e1aa8571e7ae8c03fd4d74f70
MinoHaluska <mh888mh@gmail.com>
2023-01-26 17:25:49 +0100

    Main commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AgMaterialComponent } from './ag-material/ag-material.component';
+import { BulmaComponent } from './bulma/bulma.component';
+import { SemanticComponent } from './semantic/semantic.component';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
+
+const routes: Routes = [
+  {path: 'angular', component: AgMaterialComponent},
+  {path: 'bulma', component: BulmaComponent},
+  {path: 'semantic', component: SemanticComponent},
+  {path: 'bootstrap', component: BootstrapComponent},
+  {path: '', redirectTo: '/angular', pathMatch: 'full'},
+  {path: '**', component: AgMaterialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
